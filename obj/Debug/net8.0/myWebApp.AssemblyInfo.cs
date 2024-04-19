27b351f448eeefde91ea8a5d3225065ab1e373bc
VD <30382979+vd191@users.noreply.github.com>
2024-04-20 00:08:30 +1000

    wip

--- obj/Debug/net8.0/myWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+611cf87c36806528c564b630a6e705d7b225c19f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1332b1c1cccc0cdbf60f45499d170dff2b3009bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
